968962f035191a4ea05435318cbb87922115ee6c
alec <alec.martin@goqwickly.com>
2024-01-18 14:39:42 -0500

    - lecture 36: needed to use just webdriver.FireFox webdriver.Chrome

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,27 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 
-@pytest.fixture()
-def driver():
-    print("Creating chrome driver")
-    # my_driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-    my_driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
+@pytest.fixture()
+def driver(request):
+    browser = request.config.getoption("--browser")
+    print(f"Creating {browser} driver")
+    if browser == "chrome":
+        my_driver = webdriver.Chrome()
+    elif browser == "firefox":
+        my_driver = webdriver.Firefox()
+    else:
+        raise TypeError(f"Expected 'chrome' or 'firefox', but got {browser}")
     yield my_driver
-    print("Closing chrome driver")
+    print(f"Closing {browser} driver")
     my_driver.quit()
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser", action="store", default="chrome", help="browser to execute tests (chrome or firefox)"
+    )
